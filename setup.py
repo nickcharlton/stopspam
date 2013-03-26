fab7a4f5d3612506c115c444dc08e407b9bf0651
Paul Hallett <paulandrewhallett@gmail.com>
2013-03-26 13:44:31 +0000

    Updates to fix royal-mess up.

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+import os
 
-setup(
-    name="stopspam",
-    packages=["stopspam"],
-    version="0.2",
-    description="Detect spam. Stop spam. Simple",
-    author="Paul Hallett",
-    author_email="hello@phalt.co",
-    url="http://github.com/phalt/stopspam",
-    download_url="https://github.com/phalt/stopspam/archive/master.zip",
-    keywords=["spam"],
-    classifiers=[
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'readme.txt')).read()
+NEWS = open(os.path.join(here, 'news.txt')).read()
+
+install_requires = [
+]
+
+
+setup(name='stopspam',
+      version='0.2.2',
+      description="Detect spam. Stop Spam. Simple",
+      long_description=README + '\n\n' + NEWS,
+      classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Development Status :: 3 - Alpha",
@@ -22,23 +25,15 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Site Management",
         "Topic :: Utilities",
     ],
-    long_description="""\
-Universal character encoding detector
--------------------------------------
-
-Detects
- - ASCII, UTF-8, UTF-16 (2 variants), UTF-32 (4 variants)
- - Big5, GB2312, EUC-TW, HZ-GB-2312, ISO-2022-CN (Traditional and Simplified Chinese)
- - EUC-JP, SHIFT_JIS, ISO-2022-JP (Japanese)
- - EUC-KR, ISO-2022-KR (Korean)
- - KOI8-R, MacCyrillic, IBM855, IBM866, ISO-8859-5, windows-1251 (Cyrillic)
- - ISO-8859-2, windows-1250 (Hungarian)
- - ISO-8859-5, windows-1251 (Bulgarian)
- - windows-1252 (English)
- - ISO-8859-7, windows-1253 (Greek)
- - ISO-8859-8, windows-1255 (Visual and Logical Hebrew)
- - TIS-620 (Thai)
-
-This version has been tested on Python 2.7.
-"""
+    keywords='spam',
+    author='Paul Hallett',
+    author_email='hello@phalt.co',
+    url='http://github.com/phalt/stopspam',
+    license='GPL3',
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['stopspam=stopspam:main']
+    }
 )
