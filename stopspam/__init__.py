99ba08b11ff0fd56a2bc204630d50dacc8798998
Nick Charlton <hello@nickcharlton.net>
2013-04-02 22:11:57 +0100

    Restructures the package __init__.py
    This follows the PEP 396 convention of versioning, putting it in a __version__
    variable.
    
    It also adds a better docstring so it's nicer with IPython.

--- stopspam/__init__.py
+++ stopspam/__init__.py
@@ -1,5 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+stopspam
+
+A library to check IP addresses, usernames and email addresses for their
+potential as spam against the http://stopforumspam.com/ database.
+
+Copyright: (c) 2013 by Paul Hallet
+License: GPL v3
+"""
+
+__title__ = 'stopspam'
+__version__ = '0.4'
+__author__ = 'Paul Hallet'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2013 Paul Hallet'
+
 import stopspam
-
-
-def version():
-    print stopspam.version()
